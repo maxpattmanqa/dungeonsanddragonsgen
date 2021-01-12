712c3d9aff2824585ef5ff9ef81e80f2409dd583
maxpattmanqa <mpattman@qa.com>
2021-01-12 21:36:39 +0000

    docker-compose configured , first api rudimentry tests completed , i

--- app_core/app.py
+++ app_core/app.py
@@ -10,10 +10,23 @@ def helloworld():
     return "HELLO WORLD !"
 
 #Api resonse test for generate weapon 
+
 @app.route('/generate_weapon')
 def generate_weapon():
-    response = requests.get(server_url+'')
-    return 
+    response = requests.get(server_url+':5002/get/text')
+    return str(response.text) + "Successful Ping "
+
+@app.route('/generate_race')
+def generate_race():
+    response = requests.get(server_url+':5003/get/text')
+    return str(response.text) + "Successful Ping "
+
+@app.route('/generate_role')
+def generate_role():
+    response = requests.get(server_url+':5001/get/text')
+    return str(response.text) + "Successful Ping "
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5100)
