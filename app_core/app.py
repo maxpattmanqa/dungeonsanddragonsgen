37e0e17cba0b62415d5e9847ebad411b4b50f025
maxpattmanqa <mpattman@qa.com>
2021-01-12 20:01:47 +0000

    Composition complete , API's need to be tested

--- app_core/app.py
+++ app_core/app.py
@@ -1,9 +1,9 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
-@app.route('/number', methods=['GET'])
-def number():
-    return 3
+@app.route('/')
+def helloworld():
+    return "HELLO WORLD !"
 
 if __name__ == '__main__':
-    app.run( host='0.0.0.0',port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5100)
