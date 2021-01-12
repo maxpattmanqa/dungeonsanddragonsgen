85b7d4c54bc81da9efcd6c0a02f9b64696310392
maxpattmanqa <mpattman@qa.com>
2021-01-12 20:45:36 +0000

    couple of little api tests

--- app_core/app.py
+++ app_core/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
-
+import requests
+from requests import get
 app = Flask(__name__)
+ip = get('https://api.ipify.org').text
+server_url = 'http://'+ip
+
 @app.route('/')
 def helloworld():
     return "HELLO WORLD !"
+
+#Api resonse test for generate weapon 
+@app.route('/generate_weapon')
+def generate_weapon():
+    response = requests.get(server_url+'')
+    return 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5100)
