de8b2741872467b651af3b8f4309245c2d4192c8
maxpa_qzrio <mpattman@qa.com>
2021-01-19 15:20:43 +0000

    testing script testing

--- jenkins/shell-script/test.sh
+++ jenkins/shell-script/test.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 echo "This is the Test Script"
+
+# Install apt dependencies
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-venv unzip chromium-browser
+
+# Install chromedriver
+rm -rf $CHROMEDRIVER_PATH
+mkdir $CHROMEDRIVER_PATH
+wget -P $CHROMEDRIVER_PATH https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+unzip $CHROMEDRIVER_PATH/chromedriver_linux64.zip -d /home/jenkins/chromedriver
+
+# Create/activate Python virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install pip requirements
+pip3 install -r requirements_for_testing.txt
+pip3 install pytest pytest-cov
+
+# Run pytest
+pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
