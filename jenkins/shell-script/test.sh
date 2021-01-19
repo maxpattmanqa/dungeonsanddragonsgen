c0e97b03485db90bd23eedad8b4e8ddbb6da118b
maxpa_qzrio <mpattman@qa.com>
2021-01-19 15:29:08 +0000

    changes to test.sh

--- jenkins/shell-script/test.sh
+++ jenkins/shell-script/test.sh
@@ -6,10 +6,10 @@ sudo apt-get update
 sudo apt-get install -y python3-pip python3-venv unzip chromium-browser
 
 # Install chromedriver
-rm -rf $CHROMEDRIVER_PATH
-mkdir $CHROMEDRIVER_PATH
-wget -P $CHROMEDRIVER_PATH https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip $CHROMEDRIVER_PATH/chromedriver_linux64.zip -d /home/jenkins/chromedriver
+#rm -rf $CHROMEDRIVER_PATH
+#mkdir $CHROMEDRIVER_PATH
+#wget -P $CHROMEDRIVER_PATH https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+#unzip $CHROMEDRIVER_PATH/chromedriver_linux64.zip -d /home/jenkins/chromedriver
 
 # Create/activate Python virtual environment
 python3 -m venv venv
