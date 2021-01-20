59649dbc1b271f3e91d2fa2694999a2e82eecb59
maxpa_qzrio <mpattman@qa.com>
2021-01-20 15:47:08 +0000

    infra debug

--- jenkins/ansible-script/deploy_stack.sh
+++ jenkins/ansible-script/deploy_stack.sh
@@ -4,7 +4,7 @@ echo "This is the Deploy Docker Stack"
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-manager << EOF
    
-    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml todo-stack
+    sudo docker stack deploy --compose-file /home/jenkins/docker-compose.yaml stack-project
 EOF 
 
 #export DATABASE_URI=${DATABASE_URI}
