30d8033eedb04b227b9bd9ed74515165aa3654e2
maxpa_qzrio <mpattman@qa.com>
2021-01-20 16:23:36 +0000

    infrastructure debugging

--- jenkins/ansible-script/deploy_stack.sh
+++ jenkins/ansible-script/deploy_stack.sh
@@ -4,6 +4,7 @@ echo "This is the Deploy Docker Stack"
 #export SECRET_KEY=${SECRET_KEY}
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-manager << EOF
+sudo usermod -aG docker ${USER}
 sudo docker stack deploy --compose-file /home/jenkins/docker-compose.yaml stack-project
 EOF 
 
