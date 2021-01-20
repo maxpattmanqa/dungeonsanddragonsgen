c74e29141b186769141c79dbbc4b585212c31ed4
maxpa_qzrio <mpattman@qa.com>
2021-01-20 10:34:40 +0000

    infrastructure jenkins changes

---
+++ jenkins/ansible-script/deploy_stack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "This is the Deploy Docker Stack"
+
+#!/bin/bash
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master << EOF
+    export DATABASE_URI=${DATABASE_URI}
+    export SECRET_KEY=${SECRET_KEY}
+    docker stack deploy --compose-file /home/jenkins/docker-compose.yaml todo-stack
+EOF
--- jenkins/ansible-script/install-ansible.sh
+++ jenkins/ansible-script/install-ansible.sh
@@ -1,9 +1,15 @@
-
+#!/bin/bash
+echo "switching user to jenkins"
+su - jenkins
 echo "installing ansible"
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 ## install ansible with pip
+##change user to jenkinks 
+#----------
+
+
 pip install --user ansible
 # check that ansible has been installed
 ansible --version
