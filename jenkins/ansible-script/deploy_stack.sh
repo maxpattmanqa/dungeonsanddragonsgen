f4041d0b20d4659a460959b1020e02f338e20ef8
maxpa_qzrio <mpattman@qa.com>
2021-01-21 17:56:45 +0000

    changes made to sh script

--- jenkins/ansible-script/deploy_stack.sh
+++ jenkins/ansible-script/deploy_stack.sh
@@ -1,8 +1,7 @@
 
 #!/bin/bash
 echo "This is the Deploy Docker Stack"
-#export DATABASE_URI=${DATABASE_URI}
-#export SECRET_KEY=${SECRET_KEY}
+
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-manager << EOF
 sudo usermod -aG docker ${USER}
