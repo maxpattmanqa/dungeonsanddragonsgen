eb0f4ff336de8d4c0befb5aedab9d404e1d91214
maxpa_qzrio <mpattman@qa.com>
2021-01-23 00:15:38 +0000

    database integration 1

--- jenkins/ansible-script/deploy_stack.sh
+++ jenkins/ansible-script/deploy_stack.sh
@@ -4,6 +4,8 @@ echo "This is the Deploy Docker Stack"
 
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-manager << EOF    
+    export DATABASE_URI=${DATABASE_URI}
+    export SECRET_KEY=${SECRET_KEY}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml stack-project
 EOF
 
