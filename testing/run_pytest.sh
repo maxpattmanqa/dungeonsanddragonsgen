da3d21a2dca3a2bf53779ebf162135f0adc08f10
maxpa_qzrio <mpattman@qa.com>
2021-01-24 17:39:40 +0000

    ds

--- testing/run_pytest.sh
+++ testing/run_pytest.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 apt-get install python3
 apt-get install python3-pip
@@ -7,35 +6,35 @@ cd ../app_core
 python3 -m venv venv 
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../weapon_generator
 python3 -m venv venv 
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=api --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../race_generator
 python3 -m venv venv 
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=api --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../role_generator
 python3 -m venv venv 
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=api --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../rating_generator
 python3 -m venv venv 
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m pytest --cov=api --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../testing
