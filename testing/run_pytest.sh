9db479eb4f2776608b4150fda7be1fdc2cb648a5
maxpa_qzrio <mpattman@qa.com>
2021-01-24 17:57:03 +0000

    ds

--- testing/run_pytest.sh
+++ testing/run_pytest.sh
@@ -6,6 +6,7 @@ sudo apt-get install python3-pip
 cd ../app_core
 python3 -m venv venv 
 source venv/bin/activate
+pip3 install pytest pytest-cov
 pip3 install -r requirements.txt
 python3 -m pytest --cov application #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
@@ -13,6 +14,7 @@ deactivate
 cd ../weapon_generator
 python3 -m venv venv 
 source venv/bin/activate
+pip3 install pytest pytest-cov
 pip3 install -r requirements.txt
 python3 -m pytest --cov api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
@@ -20,6 +22,7 @@ deactivate
 cd ../race_generator
 python3 -m venv venv 
 source venv/bin/activate
+pip3 install pytest pytest-cov
 pip3 install -r requirements.txt
 python3 -m pytest --cov api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
@@ -28,12 +31,14 @@ cd ../role_generator
 python3 -m venv venv 
 source venv/bin/activate
 pip3 install -r requirements.txt
+pip3 install pytest pytest-cov
 python3 -m pytest --cov api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../rating_generator
 python3 -m venv venv 
 source venv/bin/activate
+pip3 install pytest pytest-cov
 pip3 install -r requirements.txt
 python3 -m pytest --cov api #--cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
