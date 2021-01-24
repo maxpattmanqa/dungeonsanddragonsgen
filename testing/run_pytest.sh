e0b09df974538fa7ce56c0a267fadd1f2ff19a71
maxpa_qzrio <mpattman@qa.com>
2021-01-24 18:11:04 +0000

    ds

--- testing/run_pytest.sh
+++ testing/run_pytest.sh
@@ -6,40 +6,40 @@ sudo apt-get install python3-pip
 cd ../app_core
 python3 -m venv venv 
 source venv/bin/activate
-pip3 install pytest pytest-cov
-pip3 install -r requirements.txt
+pip3 install pytest pytest-cov --quiet
+pip3 install -r requirements.txt    --quiet
 python3 -m pytest --cov application --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../weapon_generator
 python3 -m venv venv 
 source venv/bin/activate
-pip3 install pytest pytest-cov
-pip3 install -r requirements.txt
+pip3 install pytest pytest-cov   --quiet
+pip3 install -r requirements.txt --quiet
 python3 -m pytest --cov api --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../race_generator
 python3 -m venv venv 
 source venv/bin/activate
-pip3 install pytest pytest-cov
-pip3 install -r requirements.txt
+pip3 install pytest pytest-cov --quiet
+pip3 install -r requirements.txt --quiet
 python3 -m pytest --cov api --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../role_generator
 python3 -m venv venv 
 source venv/bin/activate
-pip3 install -r requirements.txt
-pip3 install pytest pytest-cov
+pip3 install -r requirements.txt --quiet
+pip3 install pytest pytest-cov  --quiet
 python3 -m pytest --cov api --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
 cd ../rating_generator
 python3 -m venv venv 
 source venv/bin/activate
-pip3 install pytest pytest-cov
-pip3 install -r requirements.txt
+pip3 install pytest pytest-cov  --quiet
+pip3 install -r requirements.txt --quiet
 python3 -m pytest --cov api --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 
