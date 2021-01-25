0602085dc39afbfaff4b38d886eae8bc91a52e1c
maxpa_qzrio <mpattman@qa.com>
2021-01-25 18:22:05 +0000

    for the elves

--- race_generator/api/api_routes.py
+++ race_generator/api/api_routes.py
@@ -4,6 +4,6 @@ import random
 
 @app.route('/get/race_id', methods=['GET'])
 def get_race_id():
-    rand_value = random.randint(1,6)
-    rand_value
+    # rand_value = random.randint(1,6)
+    rand_value =1
     return Response(str(rand_value), mimetype='text/plain')
