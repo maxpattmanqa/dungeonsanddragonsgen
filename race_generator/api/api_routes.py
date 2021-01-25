ebb36e1f1e2c8b1c5a77e1c7841579ac5a919cf4
maxpa_qzrio <mpattman@qa.com>
2021-01-25 19:25:33 +0000

    rolling update

--- race_generator/api/api_routes.py
+++ race_generator/api/api_routes.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify, Response
 from api import app
-import random
+#import random
 
 @app.route('/get/race_id', methods=['GET'])
 def get_race_id():
-    rand_value = random.randint(1,6)
-   # rand_value =1
+#    rand_value = random.randint(1,6)
+    rand_value =1
     return Response(str(rand_value), mimetype='text/plain')
